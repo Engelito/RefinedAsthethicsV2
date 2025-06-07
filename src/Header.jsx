7e3f79364470e9ae4241edf3cda3638824ad87c7
Angel <anggon3.ag@gmail.com>
2025-06-06 22:08:46 -0700

    updated links to home

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 function NavBar(){
     return(
     <nav className="navBar">
-        <Link to='/Home' className="nav-logo">
+        <Link to='/' className="nav-logo">
         <img src={logoImage} alt="Your Company Logo" className="logo" />
         </Link>
          <ul className="navLinks">
-                <li><Link to="/Home" className='nav-link'>Home</Link></li>
+                <li><Link to="/" className='nav-link'>Home</Link></li>
                 <li><Link to="/Services" className='nav-link'>Services</Link></li>
                 <li><Link to="/Contact" className='nav-link'>Contact</Link></li>
                 <li><Link to="/Merch" className='nav-link'>Merch</Link></li>
